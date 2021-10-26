e08e49ea1bc1a517be8855767e4222b6d8755f78
Robert Andrassy <robert.andrassy@h-its.org>
2021-10-26 17:29:36 +0200

    Simplify the SLH data reader.

--- 3D/slhoutput.py
+++ 3D/slhoutput.py
@@ -22,9 +22,7 @@ import copy
 # We do not need a cryptographic hash, so this should be enough.
 from hashlib import md5 as slhhash
 
-from vector import curl, divergence
-
-import quadfloat
+from decimal import Decimal
 
 import matplotlib.pyplot as plt
 import matplotlib._cm
@@ -79,12 +77,67 @@ plt.register_cmap(name='slh',
                      plt.rcParams['image.lut']))
 plt.rc('image', cmap='slh')
 
+def divergence(f, *varargs):
+    D = np.array(np.gradient(f, *varargs)[1:])
+    return np.trace(D)
+
+def curl(f, *varargs):
+    _, dx, dy, dz = np.gradient(f, *varargs)
+    return np.array([dy[2] - dz[1], dz[0] - dx[2], dx[1] - dy[0]])
+
+def parse_nonibm_quad(s, return_float=True, endian='='):
+    """parse a floating point number in binary128 format as specified by IEEE 754-2008
+    Arguments:
+        s:            a string containing the binary representation of the floating point number (length: 16)
+        return_float: return result as a Python float (default) otherwise Decimal is used
+        endian:       can be <, >, or = (default)
+    """
+    if not isinstance(s, (str, bytes)):
+        raise TypeError("argument must be a string")
+    if len(s) != 16:
+        raise ValueError("length of input must be 16")
+
+    if endian == '=':
+        endian = '<' if np.little_endian else '>'
+
+    if endian == '<':
+        swap = slice(None,None,-1)
+    elif endian == '>':
+        swap = slice(None)
+    else:
+        raise ValueError("unknown endianness")
+
+    s = np.fromstring(s, dtype='u1')
+    bits = ''.join([np.binary_repr(i,width=8)[swap] for i in s])
+    bits = bits[swap]
+
+    sign = int(bits[0], 2)
+
+    exponent = int(bits[1:16],2)
+
+    significand = int(bits[16:],2)
+
+    if exponent == 0x7fff:
+        if significand == 0:
+            ret = Decimal(float('inf') * (-1)**sign)
+        else:
+            ret = Decimal(float('nan'))
+    elif exponent == 0:
+        ret = Decimal((sign, (1,), 0)) * Decimal(2)**(-16382) * (0 + Decimal(2)**(1-113) * significand)
+    else:
+        ret = Decimal((sign, (1,), 0)) * Decimal(2)**(exponent-16383) * (1+Decimal(2)**(1-113) * significand)
+
+    if return_float:
+        return float(ret)
+    else:
+        return ret
+
 def parse_quad(x, endian='='):
     global ibm_quad
     if ibm_quad:
         return unpack(endian+"d",x[:8])[0] + unpack(endian+"d",x[8:])[0]
     else:
-        return quadfloat.parse_quad(x, endian=endian)
+        return parse_nonibm_quad(x, endian=endian)
 
 def wrap_mproc(i):
     """wrapper function seemingly needed for multiprocessing"""
